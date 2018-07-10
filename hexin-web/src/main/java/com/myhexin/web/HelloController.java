d8530a84b624add66a8fc8fe706a4388fea55a10
sunjie <sunjie@myhexin.com>
2018-07-10 20:28:43 +0800

    添加Spring-boot

--- hexin-web/src/main/java/com/myhexin/web/HelloController.java
+++ hexin-web/src/main/java/com/myhexin/web/HelloController.java
@@ -1,5 +1,23 @@
 package com.myhexin.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@SpringBootApplication
 public class HelloController {
+
+    @RequestMapping(value = "home", method = {RequestMethod.GET, RequestMethod.POST}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String get() {
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
+
 }
